17ab60a34e9e71ac447242f4658f0d1836a3dbd8
Joachim Thorsen Larsen <joacla14@uia.no>
2022-12-06 03:06:31 +0100

    Added base project to test cppfront with aoc2022 day 1

---
+++ aoc2022/day_1_cppfront/main.cpp
@@ -0,0 +1,15 @@
+// ----- Cpp2 support -----
+#include "cpp2util.h"
+
+
+#line 1 "main.cpp2"
+[[nodiscard]] auto main() -> int;
+
+//=== Cpp2 definitions ==========================================================
+
+#line 1 "main.cpp2"
+[[nodiscard]] auto main() -> int{
+    std::cout << "Hello, World!" << std::endl;
+
+    return 1; 
+}
